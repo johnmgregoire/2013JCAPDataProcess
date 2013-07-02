e7c8ce82809e4dba85310da64cbd53a5d542ac40
Allison Schubauer <aschubauer@hmc.edu>
2013-07-02 10:31:46 -0700

    switched to using multiprocessing pool for optimal performance

--- fomautomator.py
+++ fomautomator.py
@@ -4,7 +4,7 @@
 
 import sys
 import cPickle as pickle
-from multiprocessing import Process
+from multiprocessing import Process, Pool
 from inspect import *
 from rawdataparser import *
 import jsontranslator
@@ -12,63 +12,43 @@ import importlib
 import distutils.util
 import itertools
 
-"""PARAMS = {}
-RAW_DATA = {}
-INTER_DATA = {}
-FOMS = {}
-
-FUNC_MOD = None
-ALL_FUNCS = []
-FUNC_DICTS = {}"""
-
-class FOMAutomator():
+class FOMAutomator(object):
     def __init__(self, rawDataFiles, funcModule):
-        global FUNC_MOD
-        global ALL_FUNCS
-        FUNC_MOD = __import__(funcModule)
-        ALL_FUNCS = [f[1] for f in getmembers(FUNC_MOD, isfunction)]
-        self.files = rawDataFiles
+        self.funcMod = __import__(funcModule)
         self.modname = funcModule
-        #self.module = [sys.modules[mod] for mod in sys.modules
-                       #if mod == funcModule][0]
-        #self.allFuncs = [f[1] for f in getmembers(self.modname, isfunction)]
+        self.allFuncs = [f[1] for f in getmembers(self.funcMod, isfunction)]
+        self.files = rawDataFiles
 
     def runParallel(self):
         self.processFuncs()
         self.requestParams()
-        for txtfile in self.files:
-            runner = FileRunner(txtfile, self.modname, PARAMS, FUNC_DICTS)
-            runner.start()
-            runner.join()
+        processPool = Pool()
+        jobs = [(filename, self.modname, self.params, self.funcDicts) for filename in self.files]
+        processPool.map(makeFileRunner, jobs)
 
     def runSequentially(self):
-        global PARAMS
-        global RAW_DATA
-        global INTER_DATA
-        global FOMS
         # user-input params for each function are the same for all files in this session
-        PARAMS = {}
         self.processFuncs()
         self.requestParams()
         # this is the sequential part
         for expfile in self.files:
             # intermediate data and figures of merit are different for every file
-            INTER_DATA = {}
-            FOMS = {}
+            self.interData = {}
+            self.FOMs = {}
             rawdatafile = readechemtxt(expfile)
             with open(rawdatafile) as rawdata:
-                RAW_DATA = pickle.load(rawdata)
-            validDictArgs = [RAW_DATA, INTER_DATA]
-            expType = RAW_DATA.get('BLTechniqueName')
+                self.rawData = pickle.load(rawdata)
+            validDictArgs = [self.rawData, self.interData]
+            expType = self.rawData.get('BLTechniqueName')
             print 'expType:', expType
-            targetFOMs = FUNC_MOD.validFuncs.get(expType)
+            targetFOMs = self.funcMod.validFuncs.get(expType)
             if not targetFOMs:
                 print expfile
                 continue
-            fomFuncs = [func for func in ALL_FUNCS if func.func_name in targetFOMs]
+            fomFuncs = [func for func in self.allFuncs if func.func_name in targetFOMs]
             for funcToRun in fomFuncs:
                 fname = funcToRun.func_name
-                fdict = FUNC_DICTS[fname]
+                fdict = self.funcDicts[fname]
                 fdictargs = validDictArgs[:fdict['numdictargs']]
                 allvarsets = [fdict.get('~'+batch) for batch in fdict.get('batchvars')]
                 commonvars = lambda vartup, varlist: [var for var in varlist if
@@ -85,16 +65,14 @@ class FOMAutomator():
                         fom = funcToRun(**dict(zip(funcToRun.func_code.co_varnames[:funcToRun.func_code.co_argcount],
                                                     fdictargs+[self.accessDict(fname, varset, argname) for argname
                                                     in funcToRun.func_code.co_varnames[fdict['numdictargs']:funcToRun.func_code.co_argcount]])))
-                        FOMS[('_').join(map(str, varset))+'_'+fname] = fom
+                        self.FOMs[('_').join(map(str, varset))+'_'+fname] = fom
             # temporary function to monitor program's output
             self.testWithJSON(expfile)
 
     def processFuncs(self):
-        global PARAMS
-        global FUNC_DICTS
-        PARAMS = {}
-        FUNC_DICTS = {}
-        for func in ALL_FUNCS:
+        self.params = {}
+        self.funcDicts = {}
+        for func in self.allFuncs:
             fname = func.func_name
             funcdict = {'batchvars': [], 'params': []}
             dictargs = func.func_code.co_argcount - len(func.func_defaults)
@@ -111,15 +89,14 @@ class FOMAutomator():
                 elif isinstance(val, str):
                     funcdict[arg] = val
                 else:
-                    PARAMS[fname+'_'+arg] = val
+                    self.params[fname+'_'+arg] = val
                     funcdict['params'].append(arg)
                     funcdict['#'+arg] = val
-            FUNC_DICTS[fname] = funcdict
+            self.funcDicts[fname] = funcdict
 
     def requestParams(self):
-        global PARAMS
-        for fname in FUNC_DICTS.keys():
-            fdict = FUNC_DICTS[fname]
+        for fname in self.funcDicts.keys():
+            fdict = self.funcDicts[fname]
             if fdict.get('params'):
                 #usedefault = distutils.util.strtobool(raw_input("Use default parameters for "+fname+"? "))
                 # uncomment ^ this line to take parameters from user
@@ -127,20 +104,20 @@ class FOMAutomator():
                 if not usedefault:
                     for param in fdict.get('params'):
                         newval = raw_input(param+": ")
-                        PARAMS[fname+'_'+param] = attemptnumericconversion(newval)
+                        self.params[fname+'_'+param] = attemptnumericconversion(newval)
                         fdict['#'+param] = attemptnumericconversion(newval)
 
     def accessDict(self, fname, varset, argname):
-        fdict = FUNC_DICTS.get(fname)
+        fdict = self.funcDicts.get(fname)
         try:
             # parameter
-            return PARAMS[fname+'_'+argname]
+            return self.params[fname+'_'+argname]
         except KeyError:
             if argname in fdict['batchvars']:
                 # retrieve current variable in batch
                 datavar = [var for var in varset if var in fdict['~'+argname]][0]
                 return datavar
-            elif (fdict[argname] in RAW_DATA) or (fdict[argname] in INTER_DATA):
+            elif (fdict[argname] in self.rawData) or (fdict[argname] in self.interData):
                 # raw/intermediate data value
                 return fdict[argname]
             else:
@@ -151,40 +128,39 @@ class FOMAutomator():
 
     def testWithJSON(self, expfilepath):
         expfilename = os.path.split(expfilepath)[1][:-4]
-        savepath = jsontranslator.toJSON(expfilename+'v1', FOMS, INTER_DATA, PARAMS)
+        savepath = jsontranslator.toJSON(expfilename+'v1',
+                                         self.FOMs, self.interData, self.params)
         jsontranslator.fromJSON(savepath)
 
-class FileRunner(Process):
+def makeFileRunner(args):
+    return FileRunner(*args)
+
+class FileRunner(object):
     def __init__(self, expfile, modname, paramdict, funcdicts):
-        super(FileRunner, self).__init__()
         self.txtfile = expfile
         self.modname = modname
         self.params = paramdict
         self.fdicts = funcdicts
+        self.run()
 
     def run(self):
-        global FUNC_MOD
-        global ALL_FUNCS
-        global RAW_DATA
-        global INTER_DATA
-        global FOMS
-        FUNC_MOD = __import__(self.modname)
-        ALL_FUNCS = [f[1] for f in getmembers(FUNC_MOD, isfunction)]
+        funcMod = __import__(self.modname)
+        allFuncs = [f[1] for f in getmembers(funcMod, isfunction)]
         # RUN ON ONE FILE
         # intermediate data and figures of merit are different for every file
-        INTER_DATA = {}
-        FOMS = {}
+        self.interData = {}
+        self.FOMs = {}
         rawdatafile = readechemtxt(self.txtfile)
         with open(rawdatafile) as rawdata:
-            RAW_DATA = pickle.load(rawdata)
-        validDictArgs = [RAW_DATA, INTER_DATA]
-        expType = RAW_DATA.get('BLTechniqueName')
+            self.rawData = pickle.load(rawdata)
+        validDictArgs = [self.rawData, self.interData]
+        expType = self.rawData.get('BLTechniqueName')
         print 'expType:', expType
-        targetFOMs = FUNC_MOD.validFuncs.get(expType)
+        targetFOMs = funcMod.validFuncs.get(expType)
         if not targetFOMs:
             print self.txtfile
             return
-        fomFuncs = [func for func in ALL_FUNCS if func.func_name in targetFOMs]
+        fomFuncs = [func for func in allFuncs if func.func_name in targetFOMs]
         for funcToRun in fomFuncs:
             fname = funcToRun.func_name
             fdict = self.fdicts[fname]
@@ -204,7 +180,7 @@ class FileRunner(Process):
                     fom = funcToRun(**dict(zip(funcToRun.func_code.co_varnames[:funcToRun.func_code.co_argcount],
                                                 fdictargs+[self.accessDict(fname, varset, argname) for argname
                                                 in funcToRun.func_code.co_varnames[fdict['numdictargs']:funcToRun.func_code.co_argcount]])))
-                    FOMS[('_').join(map(str, varset))+'_'+fname] = fom
+                    self.FOMs[('_').join(map(str, varset))+'_'+fname] = fom
         # temporary function to monitor program's output
         self.testWithJSON(self.txtfile)
 
@@ -218,7 +194,7 @@ class FileRunner(Process):
                 # retrieve current variable in batch
                 datavar = [var for var in varset if var in fdict['~'+argname]][0]
                 return datavar
-            elif (fdict[argname] in RAW_DATA) or (fdict[argname] in INTER_DATA):
+            elif (fdict[argname] in self.rawData) or (fdict[argname] in self.interData):
                 # raw/intermediate data value
                 return fdict[argname]
             else:
@@ -229,5 +205,6 @@ class FileRunner(Process):
 
     def testWithJSON(self, expfilepath):
         expfilename = os.path.split(expfilepath)[1][:-4]
-        savepath = jsontranslator.toJSON(expfilename+'v1', FOMS, INTER_DATA, self.params)
+        savepath = jsontranslator.toJSON(expfilename+'v1',
+                                         self.FOMs, self.interData, self.params)
         jsontranslator.fromJSON(savepath)
